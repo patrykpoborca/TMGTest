fd8a234d7481d38f813a64a305b599416eeaf3e6
Patryk Poborca <tryme219@gmail.com>
2022-11-21 10:56:37 -0600

    initial commit

---
+++ app/src/main/java/com/patryk/tmgtest/TMGApp.kt
@@ -0,0 +1,101 @@
+package com.patryk.tmgtest
+
+import android.app.Application
+
+class TMGApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
+
+//https://gist.githubusercontent.com/patrykpoborca/5f8abb88040d70f57528c9aa0364fecb/raw/9ee6968b94df0594d1a79335c1c0f3f1ffceeaee/tmgdata.json
+/*
+[
+   {
+      "p1":"Amos",
+      "score1":4,
+      "p2":"Diego",
+      "score2":5
+   },
+   {
+      "p1":"Amos",
+      "score1":1,
+      "p2":"Diego",
+      "score2":5
+   },
+   {
+      "p1":"Amos",
+      "score1":2,
+      "p2":"Diego",
+      "score2":5
+   },
+   {
+      "p1":"Amos",
+      "score1":0,
+      "p2":"Diego",
+      "score2":5
+   },
+   {
+      "p1":"Amos",
+      "score1":6,
+      "p2":"Diego",
+      "score2":5
+   },
+   {
+      "p1":"Amos",
+      "score1":5,
+      "p2":"Diego",
+      "score2":2
+   },
+   {
+      "p1":"Amos",
+      "score1":4,
+      "p2":"Diego",
+      "score2":0
+   },
+   {
+      "p1":"Joel",
+      "score1":4,
+      "p2":"Diego",
+      "score2":5
+   },
+   {
+      "p1":"Tim",
+      "score1":4,
+      "p2":"Amos",
+      "score2":5
+   },
+   {
+      "p1":"Tim",
+      "score1":5,
+      "p2":"Amos",
+      "score2":2
+   },
+   {
+      "p1":"Amos",
+      "score1":3,
+      "p2":"Tim",
+      "score2":5
+   },
+   {
+      "p1":"Amos",
+      "score1":5,
+      "p2":"Tim",
+      "score2":3
+   },
+   {
+      "p1":"Amos",
+      "score1":5,
+      "p2":"Joel",
+      "score2":4
+   },
+   {
+      "p1":"Joel",
+      "score1":5,
+      "p2":"Tim",
+      "score2":2
+   }
+]
+ */
---
+++ app/src/main/java/com/patryk/tmgtest/di/AppComponent.kt
@@ -0,0 +1,10 @@
+package com.patryk.tmgtest.di
+
+import dagger.Component
+import javax.inject.Singleton
+
+@Singleton
+@Component()
+class AppComponent {
+
+}
